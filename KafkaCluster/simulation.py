757f8ba1431c4b9343fb447b9343e5c80ea42b4e
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-05-30 14:41:57 +0700

    feat: implement CLI script to simulate meter data stream to Kafka

---
+++ KafkaCluster/simulation.py
@@ -0,0 +1,48 @@
+from meter import Meter
+from argparse import ArgumentParser
+from threading import Thread
+
+
+def parse_arg():
+    parser = ArgumentParser(
+        prog="Meter Data Sending Simulation",
+        description="Simulate sending meter data to Kafka",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--district",
+        type=str,
+        required=False,
+        help="District name, e.g., district1, district2, etc.",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    num_meters = 5
+    wards = {"ward1": "01", "ward2": "02", "ward3": "03"}
+    district = {"district1": "01", "district2": "02", "district3": "03"}
+
+    args = parse_arg()
+
+    if args.district:
+        district = {args.district: district[args.district]}
+
+    threads = []
+    for d in district.keys():
+        for w in wards.keys():
+            for i in range(num_meters):
+                # print(f"{district[d]}-{wards[w]}-000{i+1}")
+                meter = Meter(f"{district[d]}-{wards[w]}-000{i+1}", w)
+                thread = Thread(target=meter.send_data)
+                threads.append(thread)
+                thread.start()
+
+    [t.join() for t in threads]
+    print("im done, kms")
+
+
+if __name__ == "__main__":
+    main()
